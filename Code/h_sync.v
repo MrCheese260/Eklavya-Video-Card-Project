54b3f3529ab4708e87bc2ae54a90dc8f3d8a7212
SarveshGanu <76723054+MrCheese260@users.noreply.github.com>
2024-07-19 16:02:50 +0530

    Update h_sync.v

--- Code/h_sync.v
+++ Code/h_sync.v
@@ -1,4 +1,4 @@
-`timescale 1ns/1ps
+`timescale 1us/1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
@@ -23,17 +23,27 @@
 module h_sync(
     input clk,
     output reg h_sync_in,
+   // output reg video_timing,
     output reg v_sync_in,
     output reg [9:0] reset_count
     );
+    reg new_clk;
     reg [10:0] count;
+    reg [1:0] temp;
     initial begin
+       // video_timing <= 1;
         reset_count <= 0;
         count <= 0;
         h_sync_in <= 1;
         v_sync_in <= 1;
+        temp <= 0;
+        new_clk <= 0;
         end
-    always @(posedge clk) begin
+  always @ (posedge clk) begin
+        new_clk <= ~new_clk;
+     end
+            
+    always @(posedge new_clk) begin
        count <= count + 1;
         case (count)
         800: h_sync_in <= 1;
@@ -42,12 +52,12 @@ module h_sync(
         1040: begin 
                 count <= 0;
                 reset_count <= reset_count + 1;
+                 case(reset_count)
+                        637: v_sync_in <= 0;                            
+                        643: v_sync_in <= 1;  
+                        666: reset_count <= 0; 
+                   endcase
               end    
-        endcase
-        if (reset_count >= 637 && reset_count < 644)
-            v_sync_in <= 0;
-        else
-            v_sync_in <= 1;            
+        endcase        
     end
-    
 endmodule
