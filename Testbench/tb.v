6b246225a38b7866bb71b720d1a5414a273a5ef3
Suchit <65606811+IamLegend509@users.noreply.github.com>
2024-08-03 14:46:48 +0530

    Add files via upload
    Testbench file for Image testing

---
+++ Testbench/tb.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 31.07.2024 16:34:46
+// Design Name: 
+// Module Name: test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb();
+ wire [23:0] test_mem;
+ image_test uut (
+    .test_mem(test_mem)
+ );
+ 
+endmodule
