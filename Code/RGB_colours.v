db255c22062ce5278799c5f2067b82f3163fbab5
Suchit <65606811+IamLegend509@users.noreply.github.com>
2024-08-13 19:55:27 +0530

    Scripts for displaying 200x150 Resolution image

--- Code/RGB_colours.v
+++ Code/RGB_colours.v
@@ -7,87 +7,54 @@ module RGB_colours(
     input [10:0] count_rgb,
     output reg [7:0] red_1,
     output reg [7:0] green_1,
-    output reg [7:0] blue_1,
-    
-    output reg [7:0] red_2,
-    output reg [7:0] green_2,
-    output reg [7:0] blue_2,
-    
-    output reg [7:0] red_3,
-    output reg [7:0] green_3,
-    output reg [7:0] blue_3,
-    
-    output reg [7:0] red_4,
-    output reg [7:0] green_4,
-    output reg [7:0] blue_4
+    output reg [7:0] blue_1
     );
     reg [9:0] i;
     reg [9:0] j;
     //Slice1
-    reg [7:0] red1 [0:799][0:149];
-    reg [7:0] green1 [0:799][0:149];
-    reg [7:0] blue1 [0:799][0:149];
+    reg [7:0] red1 [0:199][0:149];
+    reg [7:0] green1 [0:199][0:149];
+    reg [7:0] blue1 [0:199][0:149];
     //Slice2
-    reg [7:0] red2 [0:799][0:149];
-    reg [7:0] green2 [0:799][0:149];
-    reg [7:0] blue2 [0:799][0:149];
-    //Slice3
-    reg [7:0] red3 [0:799][0:149];
-    reg [7:0] green3 [0:799][0:149];
-    reg [7:0] blue3 [0:799][0:149];
-    //Slice4
-    reg [7:0] red4 [0:799][0:149];
-    reg [7:0] green4 [0:799][0:149];
-    reg [7:0] blue4 [0:799][0:149];
+//    reg [7:0] red2 [0:199][0:149];
+//    reg [7:0] green2 [0:199][0:149];
+//    reg [7:0] blue2 [0:199][0:149];
+//    //Slice3
+//    reg [7:0] red3 [0:199][0:149];
+//    reg [7:0] green3 [0:199][0:149];
+//    reg [7:0] blue3 [0:199][0:149];
+//    //Slice4
+//    reg [7:0] red4 [0:199][0:149];
+//    reg [7:0] green4 [0:199][0:149];
+//    reg [7:0] blue4 [0:199][0:149];
     
     
     initial begin
    //Slice1
-        $readmemh("C:/Users/sarve/Downloads/output_hex_array_top_1_G.hex", green1);
-        $readmemh("C:/Users/sarve/Downloads/output_hex_array_top_1_B.hex", blue1);
-        $readmemh("C:/Users/sarve/Downloads/output_hex_array_top_1_R.hex", red1);
+        $readmemh("C:/Vivado/h_sync/new_output_G.hex", green1);
+        $readmemh("C:/Vivado/h_sync/new_output_B.hex", blue1);
+        $readmemh("C:/Vivado/h_sync/new_output_R.hex", red1);
    //Slice2
-        $readmemh("C:/Users/sarve/Downloads/output_hex_array_top_2_G.hex", green2);
-        $readmemh("C:/Users/sarve/Downloads/output_hex_array_top_1_B.hex", blue2);
-        $readmemh("C:/Users/sarve/Downloads/output_hex_array_top_1_R.hex", red2);
-   //Slice3
-        $readmemh("C:/Users/sarve/Downloads/output_hex_array_middle_G.hex", green3);
-        $readmemh("C:/Users/sarve/Downloads/output_hex_array_middle_B.hex", blue3);
-        $readmemh("C:/Users/sarve/Downloads/output_hex_array_middle_R.hex", red3);
-   //Slice4
-        $readmemh("C:/Users/sarve/Downloads/output_hex_array_bottom_G.hex", green4);
-        $readmemh("C:/Users/sarve/Downloads/output_hex_array_bottom_B.hex", blue4);
-        $readmemh("C:/Users/sarve/Downloads/output_hex_array_bottom_R.hex", red4);
-        
+//        $readmemh("C:/Vivado/h_sync/output_hex_array_top_2_G.hex", green2);
+//        $readmemh("C:/Vivado/h_sync/output_hex_array_top_1_B.hex", blue2);
+//        $readmemh("C:/Vivado/h_sync/output_hex_array_top_1_R.hex", red2);
+//   //Slice3
+//        $readmemh("C:/Vivado/h_sync/output_hex_array_middle_G.hex", green3);
+//        $readmemh("C:/Vivado/h_sync/output_hex_array_middle_B.hex", blue3);
+//        $readmemh("C:/Vivado/h_sync/output_hex_array_middle_R.hex", red3);
+//   //Slice4
+//        $readmemh("C:/Vivado/h_sync/output_hex_array_bottom_G.hex", green4);
+//        $readmemh("C:/Vivado/h_sync/output_hex_array_bottom_B.hex", blue4);
+//        $readmemh("C:/Vivado/h_sync/output_hex_array_bottom_R.hex", red4);
+
         i = 0;
         j = 0;
         end
         always @(posedge clk) begin
-        if (count_rgb < 800 && reset_count_rgb < 150) begin //Slice1
+        if (count_rgb < 200 && reset_count_rgb < 150) begin //Slice1
             red_1 <= red1[i][j];
             green_1 <= green1[i][j];
             blue_1 <= blue1[i][j];
-            i = i + 1;
-        end
-        
-        else if(count_rgb < 800 && reset_count_rgb < 300 && reset_count_rgb >150) begin //Slice 2
-            red_2 <= red2[i][j];
-            green_2 <= green2[i][j];
-            blue_2 <= blue2[i][j];
-            i = i + 1;
-        end
-        
-        else if (count_rgb < 800 && reset_count_rgb < 450 && reset_count_rgb >300) begin //Slice 3
-            red_3 <= red3[i][j];
-            green_3 <= green3[i][j];
-            blue_3 <= blue3[i][j];
-            i = i + 1;
-        end
-        
-        else if (count_rgb < 800 && reset_count_rgb < 600 && reset_count_rgb >450) begin //Slice 4
-            red_4 <= red4[i][j];
-            green_4 <= green4[i][j];
-            blue_4 <= blue4[i][j];
             i = i + 1;
         end
         
--- Code/h_sync.v
+++ Code/h_sync.v
@@ -1,9 +1,9 @@
-`timescale 1us/1ps
+`timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
 // 
-// Create Date: 16.07.2024 12:45:23
+// Create Date: 03.08.2024 15:45:30
 // Design Name: 
 // Module Name: h_sync
 // Project Name: 
@@ -21,111 +21,6 @@
 
 
 module h_sync(
-    input clk,
-    input wire [31:0] red,
-    input wire [31:0] green,
-    input wire [31:0] blue,
-    output reg h_sync_in,
-    output reg v_sync_in,
-    output reg [9:0] reset_count,
-    output reg [10:0] count,
-    output reg [1:0] red_port,
-    output reg [1:0] green_port,
-    output reg [1:0] blue_port
+
     );
-    reg new_clk;
-    initial begin
-        reset_count <= 0;
-        count <= 0;
-        h_sync_in <= 1;
-        v_sync_in <= 1;
-        new_clk <= 0;
-    end 
-    
-    always @(posedge clk) begin
-        new_clk <= ~new_clk;
-     end
-          
-    always @(posedge new_clk) begin
-       count <= count + 1;
-        case (count)
-        800: h_sync_in <= 1;
-        856: h_sync_in <= 0;
-        976: h_sync_in <= 1;
-        1040: begin
-                count <= 0;
-                reset_count <= reset_count + 1;
-                 case(reset_count)
-                        637: v_sync_in <= 0;                            
-                        643: v_sync_in <= 1;  
-                        666: reset_count <= 0; 
-                  endcase
-              end    
-        endcase        
-    end
-    
-    RGB_colours x1 (
-        .clk(new_clk),
-        .reset_count_rgb(reset_count),
-        .count_rgb(count),
-        .red_1(red[7:0]),
-        .green_1(green[7:0]),
-        .blue_1(blue[7:0]),
-        
-        .red_2(red[15:8]),
-        .green_2(green[15:8]),
-        .blue_2(blue[15:8]),
-        
-        .red_3(red[23:15]),
-        .green_3(green[23:15]),
-        .blue_3(blue[23:15]),
-        
-        .red_4(red[31:24]),
-        .green_4(green[31:24]),
-        .blue_4(blue[31:24])
-    );
-    always @(posedge new_clk) begin
-            if (red < 8'd33) begin
-                red_port <= 2'b00;
-            end
-            else if (red > 8'd32 && red < 8'd129) begin
-                red_port <= 2'b01; 
-            end
-            else if (red > 8'd128 && red < 8'd192) begin
-                red_port <= 2'b10;
-            end
-            else begin
-                red_port <= 2'b11;
-            end
-             
-             
-             
-            if (green < 8'd33)begin
-                green_port <= 2'b00;
-            end
-            else if (green > 8'd32 && green < 8'd129) begin
-                green_port <= 2'b01; 
-            end
-            else if (green > 8'd128 && green < 8'd192)begin
-                green_port <= 2'b10;
-            end
-            else begin
-                green_port <= 2'b11;
-            end
-             
-             
-             
-            if (blue < 8'd33)begin
-                blue_port <= 2'b00;
-            end
-            else if (blue > 8'd32 && blue < 8'd129)begin
-                blue_port <= 2'b01; 
-            end
-            else if (blue > 8'd128 && blue < 8'd192)begin
-                 blue_port <= 2'b10;
-            end
-            else begin
-                 blue_port <= 2'b11;
-            end
-    end
 endmodule
