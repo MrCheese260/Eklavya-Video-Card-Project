88582e195fc1502f60b5937fef4d27abe9ef8d6f
SarveshGanu <76723054+MrCheese260@users.noreply.github.com>
2024-08-17 17:27:29 +0530

    Update RGB_colours.v

--- Code/RGB_colours.v
+++ Code/RGB_colours.v
@@ -2,6 +2,7 @@
 
 
 module  hex_loader(
+    input clk_fast,
     input clk,
     input [9:0] reset_count_rgb,
     input [10:0] count_rgb,
@@ -9,22 +10,22 @@ module  hex_loader(
     output reg [1:0] green_1,
     output reg [1:0] blue_1
     );
-    reg [15:0] k;
+    reg [16:0] k;
     reg [1:0] red [0:119999];
     reg [1:0] blue [0:119999];
     reg [1:0] green [0:119999];
     initial begin
-        $readmemh("C:/Users/sarve/new_output_g.hex", green);
-        $readmemh("C:/Users/sarve/new_output_b.hex", blue);
-        $readmemh("C:/Users/sarve/new_output_r.hex", red);
-        k = 15'd0;
+        $readmemb("C:/Users/sarve/output_g.txt", green);
+        $readmemb("C:/Users/sarve/output_b.txt", blue);
+        $readmemb("C:/Users/sarve/output_r.txt", red);
+        k = 16'd0;
         end
         always @(posedge clk) begin
         if (count_rgb < 11'd400 && reset_count_rgb < 10'd300) begin
             red_1 <= red[k];
             green_1 <= green[k];
             blue_1 <= blue[k];
-            k <= k + 15'd1;
+            k <= k + 16'd1;
         end
         else begin
             red_1 <= 2'd0;
