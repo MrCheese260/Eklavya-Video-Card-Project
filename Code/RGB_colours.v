b69adab0e94a0b883551e79aa15e4be9c45f7986
SarveshGanu <76723054+MrCheese260@users.noreply.github.com>
2024-08-17 16:57:52 +0530

    Update RGB_colours.v

--- Code/RGB_colours.v
+++ Code/RGB_colours.v
@@ -1,36 +1,35 @@
 `timescale 1ns / 1ps
 
 
-module rgb_tester(
+module  hex_loader(
     input clk,
     input [9:0] reset_count_rgb,
     input [10:0] count_rgb,
-    output reg [7:0] red_1,
-    output reg [7:0] green_1,
-    output reg [7:0] blue_1
+    output reg [1:0] red_1,
+    output reg [1:0] green_1,
+    output reg [1:0] blue_1
     );
-    reg [14:0] k;
-    reg [7:0] red1 [0:29999];
-    reg [7:0] green1 [0:29999];
-    reg [7:0] blue1 [0:29999];
-    
+    reg [15:0] k;
+    reg [1:0] red [0:119999];
+    reg [1:0] blue [0:119999];
+    reg [1:0] green [0:119999];
     initial begin
-        $readmemh("C:/Users/sarve/new_output_g.hex", green1);
-        $readmemh("C:/Users/sarve/new_output_b.hex", blue1);
-        $readmemh("C:/Users/sarve/new_output_r.hex", red1);
+        $readmemh("C:/Users/sarve/new_output_g.hex", green);
+        $readmemh("C:/Users/sarve/new_output_b.hex", blue);
+        $readmemh("C:/Users/sarve/new_output_r.hex", red);
         k = 15'd0;
         end
         always @(posedge clk) begin
-        if (count_rgb < 11'd200 && reset_count_rgb < 10'd150) begin //Slice1
-            red_1 <= red1[k];
-            green_1 <= green1[k];
-            blue_1 <= blue1[k];
+        if (count_rgb < 11'd400 && reset_count_rgb < 10'd300) begin
+            red_1 <= red[k];
+            green_1 <= green[k];
+            blue_1 <= blue[k];
             k <= k + 15'd1;
         end
         else begin
-            red_1 <= 8'd0;
-            green_1 <= 8'd0;
-            blue_1 <= 8'd0;
+            red_1 <= 2'd0;
+            green_1 <= 2'd0;
+            blue_1 <= 2'd0;
         end
       end
 endmodule
