463e469b4896ff144a283ad52a314d9508cee22f
SarveshGanu <76723054+MrCheese260@users.noreply.github.com>
2024-08-18 09:41:57 +0530

    Update RGB_colours.v
    Updated the code for interpolation of 2 Bit RGB values

--- Code/RGB_colours.v
+++ Code/RGB_colours.v
@@ -11,26 +11,61 @@ module  hex_loader(
     output reg [1:0] blue_1
     );
     reg [16:0] k;
-    reg [1:0] red [0:119999];
-    reg [1:0] blue [0:119999];
-    reg [1:0] green [0:119999];
+    reg [8:0] i,j,l,m;
+    reg [1:0] red_i [0:119999];
+    reg [1:0] blue_i [0:119999];
+    reg [1:0] green_i [0:119999];
+    reg [1:0] red [0:399][0:299];
+    reg [1:0] blue [0:399][0:299];
+    reg [1:0] green [0:399][0:299];
     initial begin
         $readmemb("C:/Users/sarve/output_g.txt", green);
         $readmemb("C:/Users/sarve/output_b.txt", blue);
         $readmemb("C:/Users/sarve/output_r.txt", red);
         k = 16'd0;
-        end
-        always @(posedge clk) begin
+        i = 8'd0;
+        j = 8'd0;
+    end
+    always @(posedge clk_fast) begin
+        if (i < 9'd400 && j < 9'd300) begin
+            red[i][j] <= red_i[k];
+            green[i][j] <= green_i[k];
+            blue[i][j] <= blue_i[k];
+            k = k + 16'd1;
+            i = i + 9'd1;
+         end
+         else begin
+            i = 9'd0; 
+            j = j + 9'd1;
+         end  
+    end
+    always @(posedge clk) begin
         if (count_rgb < 11'd400 && reset_count_rgb < 10'd300) begin
-            red_1 <= red[k];
-            green_1 <= green[k];
-            blue_1 <= blue[k];
-            k <= k + 16'd1;
+            if(count_rgb%2==0 && reset_count_rgb%2 == 0) begin
+                red_1 <= red[(count_rgb/2)][(reset_count_rgb/2)];
+                green_1 <= green[(count_rgb/2)][(reset_count_rgb/2)];
+                blue_1 <= blue[(count_rgb/2)][(reset_count_rgb/2)];
+            end
+            else if (count_rgb%2 == 1 && reset_count_rgb%2 ==0) begin
+                red_1 <= red[((count_rgb - 1)/2)][(reset_count_rgb/2)];
+                green_1 <= green[((count_rgb - 1)/2)][(reset_count_rgb/2)];
+                blue_1 <= blue[((count_rgb - 1)/2)][(reset_count_rgb/2)];
+            end
+            else if (count_rgb%2 == 0 && reset_count_rgb%2 == 1) begin
+                red_1 <= red[(count_rgb/2)][((reset_count_rgb - 1)/2)];
+                green_1 <= green[(count_rgb/2)][((reset_count_rgb - 1)/2)];
+                blue_1 <= blue[(count_rgb/2)][((reset_count_rgb - 1)/2)];
+            end
+             else if (count_rgb%2 == 1 && reset_count_rgb%2 == 1) begin
+                red_1 <= red[((count_rgb - 1)/2)][((reset_count_rgb - 1)/2)];
+                green_1 <= green[((count_rgb - 1)/2)][((reset_count_rgb - 1)/2)];
+                blue_1 <= blue[((count_rgb - 1)/2)][((reset_count_rgb - 1)/2)];
+             end
         end
         else begin
             red_1 <= 2'd0;
             green_1 <= 2'd0;
             blue_1 <= 2'd0;
         end
-      end
+    end
 endmodule
