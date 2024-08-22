b5c132eee468ce53ac630317b74a87df29b7abb8
SarveshGanu <76723054+MrCheese260@users.noreply.github.com>
2024-08-22 08:24:19 +0530

    Update VGA_controller.v
    Code for 3-bit VGA-Controller

--- Code/VGA_controller.v
+++ Code/VGA_controller.v
@@ -1,63 +1,56 @@
 `timescale 1us/1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.07.2024 12:45:23
-// Design Name: 
-// Module Name: h_sync
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-module h_sync(
+module top(
     input clk,
     output reg h_sync_in,
-   // output reg video_timing,
     output reg v_sync_in,
-    output reg [9:0] reset_count
+    output reg [9:0] reset_count,
+    output reg [10:0] count,
+    output wire [2:0] red_out,
+    output wire [2:0] green_out,
+    output wire [2:0] blue_out
     );
-    reg new_clk;
-    reg [10:0] count;
-    reg [1:0] temp;
+  reg new_clk;
     initial begin
-       // video_timing <= 1;
         reset_count <= 0;
         count <= 0;
         h_sync_in <= 1;
         v_sync_in <= 1;
-        temp <= 0;
         new_clk <= 0;
-        end
-  always @ (posedge clk) begin
+    end 
+    
+    always @(posedge clk) begin
         new_clk <= ~new_clk;
      end
-            
+    
+          
     always @(posedge new_clk) begin
        count <= count + 1;
         case (count)
         800: h_sync_in <= 1;
         856: h_sync_in <= 0;
         976: h_sync_in <= 1;
-        1040: begin 
+        1040: begin
                 count <= 0;
                 reset_count <= reset_count + 1;
                  case(reset_count)
-                        637: v_sync_in <= 0;                            
-                        643: v_sync_in <= 1;  
-                        666: reset_count <= 0; 
-                   endcase
+                        634: v_sync_in <= 0;                           
+                        643: v_sync_in <= 1;
+                        666:begin 
+                             reset_count <= 0;
+                            end
+                  endcase
               end    
-        endcase        
+        endcase
+          
     end
-endmodule
+    
+   hex_loader x1 (
+        .clk_fast(clk),
+        .clk(new_clk),
+        .reset_count_rgb(reset_count),
+        .count_rgb(count),
+        .red_1(red_out),
+        .green_1(green_out),
+        .blue_1(blue_out)
+   );
+   endmodule
